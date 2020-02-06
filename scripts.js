55362b277005ef7a7ac673f8fcbf4f09584ee4db
fcato <55493289+fcato@users.noreply.github.com>
2020-02-05 19:27:47 -0500

    testing things

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-alert("scripts.js has loaded!");
+let i;
+i = 1;
+while ( i < 4 ) {
+  $("#response").append("<br />" + i);
+  i = i + 1;
+}
+
+let  mastersTools, newTools;
+mastersTools = 10
+newTools = 20
+if (mastersTools < newTools) {
+console.log('Social justice has a chance because we will create new tools that dismantle oppressive and exploitative practices.');
+} else {
+  console.log('Things never change; we keep replicating oppressive and exploitative institutions.'); }
