9bd937e42ec763059e12d6446228c7ee3478a76b
fcato <55493289+fcato@users.noreply.github.com>
2020-03-18 15:16:27 -0400

    bootstrap jsem

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
+
+
 let i;
 i = 1;
-while ( i < 3 ) {
+while ( i < 5 ) {
   $("#response").append("<br />" + i);
+  i = i + 1;
+}
+
+let userInput;
+userInput = prompt("Please pick a number between 1 and 4.", "Type your answer here.");
+if userInput ( i < 4 ) {
+  $("#response").html("<br />" + i);
   i = i + 1;
 }
